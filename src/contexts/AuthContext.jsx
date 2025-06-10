cf4f23e0f5e3cec847eee2e5a8fd484d823c366b
Piyush <piyush.2024@nst.rishihood.edu.in>
2025-06-10 15:06:13 +0530

    Update

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -13,7 +13,6 @@ const AuthContext = createContext();
 export function useAuth() {
   return useContext(AuthContext);
 }
-
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(true);
